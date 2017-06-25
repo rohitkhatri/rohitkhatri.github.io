29bd54b77499d511976e6e68b097c7a1533807e3
sabareeshk <sabareeshk1991@gmail.com>
2017-06-25 11:49:47 +0530

    new changes to the text

--- editable.js
+++ editable.js
@@ -42,24 +42,40 @@ const init = (buttons) => {
 
 const generateButtons = (commands) => commands.map((command) => createButtons('btn-succes', command.cmd, icon(command)));
 
-const createButtons = (btnClass, command, iconClass) => `<span><code class="btn btn-xs ${btnClass}" zonmousedown="event.preventDefault();" onclick="applyEffect('${command}')"><i class="${iconClass}"></i></code></span>&nbsp;&nbsp;`
+const createButtons = (btnClass, command, iconClass) => `<span><code class="btn btn-xs ${btnClass}" onmousedown="event.preventDefault();" onclick="applyEffect('${command}')" id="${command}"><i class="${iconClass}"></i></code></span>&nbsp;&nbsp;`
 
 const icon = (cmd) => cmd.icon ? `fa fa-${cmd.icon} fa-2x` : "";
 
 const applyEffect = (cmd) => {
 	const val = (cmd === "insertImage")? prompt("Paste Your Link here:") : "";
 	document.execCommand(cmd, false, (val || ""));
+	checkStyleApplied(commands)
 }
 
 init(generateButtons(commands));
 
-
+//cursor location when cursor movement
 function getCursorLetter(event){
-	const backSpace = document.getElementById('editable-content');
+	const content = document.getElementById('editable-content');
 	var key = event.keyCode || event.charCode;
+	checkStyleApplied(commands);
 	if ( key == 8 || key == 46) {
 	    const caretPosition = window.getSelection().anchorOffset;
-	    const caretString = !!backSpace.innerHTML[caretPosition - 2].trim() ? backSpace.innerHTML[caretPosition - 2]: 'space';
+	    const caretString = !!content.innerHTML[caretPosition - 2].trim() ? content.innerHTML[caretPosition - 2]: 'space';
 	    console.log('%ccaretString', "color:blue;", caretString);
 	}
+}
+
+//apply styles
+function activateStyleButtons(command, value){
+    const hex = value ? "#333": "#31708f";
+    document.getElementById(command).style.color = hex;
+}
+
+//check for styles that are applied.
+function checkStyleApplied(commands){
+	commands.map(function(command){
+		const applied = document.queryCommandState(command.cmd);
+		activateStyleButtons(command.cmd, applied);
+	})
 }
